9cf5df9de61a97816283942d80e534dd95c5a657
tenglei <1037910995@qq.com>
2016-12-02 21:14:07 +0800

    add

---
+++ DynamicQAProject/WebContent/js/newEdit.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	var addNum=0;
+	selectdiv="<div";
+	selectdiv=seletdiv+" "+ "id=select"+
+	$("#addSelect").on("click", function() {
+		$("#quesList").append(selectdiv);
+		
+	});
+	$("#addFill").on("click", function() {
+		
+		
+		
+	});
+	$("#addAnswer").on("click", function() {
+		
+		
+		
+	});
+	
+
+});
--- DynamicQAProject/WebContent/js/userHome.js
+++ DynamicQAProject/WebContent/js/userHome.js
@@ -4,6 +4,7 @@
 $(document).ready(function() {
 	var friendsNum = document.getElementById("friendsNum");
 	var questionsNum = document.getElementById("questionsNum");
+	var addNum=0;
 	for (var i = 1; i < friendsNum.value; i++) {
 		var friend = "friend";
 		var block = "block";
@@ -51,5 +52,46 @@ $(document).ready(function() {
 		}
 
 	});
+	$("#myCircle").on("click", function() {
+		for (var i = 1; i < friendsNum.value; i++) {
+			var friend = "friend";
+			var block = "block";
+			friend = friend + i;
+			block = block + 1;
+			$("#friend"+i).css("display", "none");
+			$("#block"+i).css("display", "none");
+		}
+		for (var i = 1; i < questionsNum.value; i++) {
+			var userQ = "userQ";
+			userQ = userQ + i;
+			$("#userQ"+i).css("display", "none");
+		}
+		$("#userHome").css("display", "block");
+		$("#userDy").css("display", "block");
+		$("#mypadder").css("display", "block");
+		
+		
+
+	});
+	//排名，需调整放入循环
+	var numberOrder=document.getElementById("numberFirst");
+	var First = "第一名:";
+	var Second = "第二名:";
+	var Third = "第三名：";
+	numberOrder.innerHTML=First+"superman";
+	for(var i=1;i<=questionsNum.value;i++){
+		$("#checkOrder"+i).on("click", function() {
+			
+			
+			
+		});
+	}
+	
+	$(".btn-danger").on("click", function() {
+		$(this).parent().parent().parent().parent().remove();
+	});
+	
+	
+	
 
 });
